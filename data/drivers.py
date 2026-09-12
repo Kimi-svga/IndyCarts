27e645530fd49d71fd6c626239789d14671ae854
Kimi-svga <kimikimianton12@gmail.com>
2026-09-13 01:26:25 +0700

    Create drivers.py

---
+++ data/drivers.py
@@ -0,0 +1,19 @@
+"""Список пилотов IndyCar."""
+
+DRIVERS = {
+    "palou": {"name": "Alex Palou", "team": "Chip Ganassi Racing", "number": 10},
+    "dixon": {"name": "Scott Dixon", "team": "Chip Ganassi Racing", "number": 9},
+    "mclaughlin": {"name": "Scott McLaughlin", "team": "Team Penske", "number": 3},
+    "newgarden": {"name": "Josef Newgarden", "team": "Team Penske", "number": 2},
+    "power": {"name": "Will Power", "team": "Team Penske", "number": 12},
+    "herta": {"name": "Colton Herta", "team": "Andretti Global", "number": 26},
+    "ericsson": {"name": "Marcus Ericsson", "team": "Andretti Global", "number": 28},
+    "kirkwood": {"name": "Kyle Kirkwood", "team": "Andretti Global", "number": 27},
+    "oward": {"name": "Pato O'Ward", "team": "Arrow McLaren", "number": 5},
+    "rossi": {"name": "Alexander Rossi", "team": "Arrow McLaren", "number": 7},
+    "ferrucci": {"name": "Santino Ferrucci", "team": "A.J. Foyt Racing", "number": 14},
+    "rahal": {"name": "Graham Rahal", "team": "Rahal Letterman Lanigan", "number": 15},
+    "lundgaard": {"name": "Christian Lundgaard", "team": "Rahal Letterman Lanigan", "number": 45},
+    "veekay": {"name": "Rinus VeeKay", "team": "Ed Carpenter Racing", "number": 21},
+    "grosjean": {"name": "Romain Grosjean", "team": "Juncos Hollinger Racing", "number": 77},
+}
