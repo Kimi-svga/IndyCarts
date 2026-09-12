b74429c9a0cc35701a8aae882f0c90abc0367a40
Kimi-svga <kimikimianton12@gmail.com>
2026-09-13 01:17:09 +0700

    Create logger.py

---
+++ core/logger.py
@@ -0,0 +1,11 @@
+import logging
+import sys
+
+
+def setup_logger(name: str = "indycarts") -> logging.Logger:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+    return logging.getLogger(name)
