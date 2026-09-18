f8703a57361fc296efd6c28d558f3d78094c0110
Kimi-svga <kimikimianton12@gmail.com>
2026-09-19 03:21:46 +0700

    Create exception.py

---
+++ core/exception.py
@@ -0,0 +1,31 @@
+"""Свои исключения для Indy Carts."""
+
+
+class IndyCartsError(Exception):
+    """Базовая ошибка проекта."""
+    pass
+
+
+class InsufficientFunds(IndyCartsError):
+    """Недостаточно монет."""
+    pass
+
+
+class CardNotFound(IndyCartsError):
+    """Карта не найдена."""
+    pass
+
+
+class MergeFailed(IndyCartsError):
+    """Слияние не удалось."""
+    pass
+
+
+class UserNotFound(IndyCartsError):
+    """Игрок не найден."""
+    pass
+
+
+class BattleNotActive(IndyCartsError):
+    """Дуэль неактивна."""
+    pass
